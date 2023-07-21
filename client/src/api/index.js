bad540de365d8290f140028eb234234cf9815404
shreeshjain <118350651+shreeshjain@users.noreply.github.com>
2023-07-21 14:06:03 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: ''});
+const API = axios.create({ baseURL: 'https://stack-overflow-server-qt1c.onrender.com'});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
